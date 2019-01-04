18e2d11c7db02a45814f0a4f2d175d5062244329
Hannah Newman <22963771+hannnew@users.noreply.github.com>
2019-01-04 11:32:54 -0500

    finished getSuggestions and added more stuff

--- script.js
+++ script.js
@@ -2,19 +2,60 @@
 
 let corpus = ["earthpea","airfield","aabba","lol","the"];
 
-function getSuggestions(curr, patt) {
-	let words = curr.split();
+let sortedCorpus = makeSortedCorpus(corpus);
+console.log(sortedCorpus);
+
+function makeSortedCorpus(corpus)
+{
+	let sorted = {};
+	for(var i = 0; i < corpus.length; i++)
+	{
+		let word = corpus[i];
+		let binWord = cipherToBin(word);
+		if(binWord in sorted)
+		{
+			sorted[binWord].push(word);
+		}
+		else
+		{
+			sorted[binWord] = [word];
+		}
+	}
+	return sorted;
+}
+
+function getSuggestions(curr, patt)
+{
+	let txt = patt.split(" ").join("");
+	let bin = cipherToBin(txt);
+	let words = curr.split(" ");
+	let remaining = bin.substring(txt.length,bin.length);
 	if(curr.charAt(curr.length - 1) == " ")
 	{
 		let currWord = "";
+		return getFullSuggest(remaining);
 	}
 	else
 	{
 		let currWord = words[words.length - 1];
+		return getPartialSuggest(currWord, remaining);
 	}
-	console.log(cipherToBin(curr));
 }
 
+// here is stuff I haven't written yet!!!!
+function getPartialSuggest(word, patt)
+{
+	return "oof";
+}
+
+function getFullSuggest(patt)
+{
+	return "oof";
+	
+}
+
+
+// input to this function should be just letters! take out the spaces I am begging you!
 function cipherToBin(txt) {
 	let r = "";
 	let vowels = ["a","e","i","o","u","y"]
