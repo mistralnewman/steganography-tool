02ca04ad466cf15ea691c48a27a9a853acc04f35
Mistral Newman <mistralnewman@gmail.com>
2019-01-09 11:20:34 -0500

    addToTrie reimplemented with arrays

--- tries.js
+++ tries.js
@@ -5,6 +5,7 @@ function TrieNode(val, bin, ones, zeroes, end)
 	this.ones = ones;
 	this.zeroes = zeroes;
 	this.end = end;
+	
 	this.has = function(ch)
 	{
 		if(cipherToBin(ch) == "0")
@@ -27,11 +28,6 @@ function TrieNode(val, bin, ones, zeroes, end)
 	
 	this.get = function(ch)
 	{
-		console.log("get",ch);
-		if(!this.has(ch))
-		{
-			throw "trie does not contain "+ch.charCodeAt(0)+"!";
-		}
 		if(cipherToBin(ch) == "0")
 		{
 			side = this.zeroes;
@@ -40,77 +36,63 @@ function TrieNode(val, bin, ones, zeroes, end)
 		{
 			side = this.ones;
 		}
-		return side.get(ch);
+		for (let node of side)
+		{
+			if(node.val == ch)
+			{
+				return node;
+			}
+		}
+		throw "trie does not contain "+ch;
 	}
 	
 	this.getWord = function(w)
 	{
-		let i = 0;
 		let currNode = this;
-		console.log("currnode", currNode);
-		while(i < w.length)
+		while(w.length > 0)
 		{
-			currNode = currNode.get(w.charAt(i));
-			i++;
+			currNode = currNode.get(w.charAt(0));
+			w = w.substring(1);
 		}
 		return currNode;
 	}
-		
+	
+	this.addWord = function(w)
+	{
+		let currNode = this;
+		let newNode;
+		while(w.length > 0)
+		{
+			if (currNode.has(w.charAt(0)))
+			{
+				currNode = currNode.get(w.charAt(0));
+			}
+			else
+			{
+				newNode = new TrieNode(w.charAt(0), cipherToBin(w.charAt(0)), [], [], false);
+				if(cipherToBin(w.charAt(0)) == "1")
+				{
+					currNode.ones.push(newNode);
+				}
+				else
+				{
+					currNode.zeroes.push(newNode);
+				}
+				currNode = newNode;
+			}
+			w = w.substring(1);
+		}
+		currNode.end = true;
+		return this;
+	}
 }
 
-let trieCorpus = new TrieNode("", null, [], [], false);
+let trieCorpus = new TrieNode("", null, [], [], true);
 
-console.log(trieCorpus);
-
-function addToTrie(word, trie)
-{
-	let end = word.length <= 1;
-	let currChar = word.charAt(0);
-	let charBin = cipherToBin(currChar);
-	let side;
-	if(charBin == "1")
-	{
-		side = trie.ones;
-	}
-	else
-	{
-		side = trie.zeroes;
-	}
-	let i = 0;
-	while(i < side.length && side[i].val != currChar)
-	{
-		i++;
-	}
-	if(i >= side.length)
-	{
-		let newNode = new TrieNode(currChar, charBin, [], [], end);
-		if(!end)
-		{
-			newNode = addToTrie(word.substring(1), newNode);
-		}
-		side = side.push(newNode);
-		charBin == "1" ? trie.ones = side : trie.zeroes = side;
-	}
-	else
-	{
-		let node = side[i];
-		console.log("node", node);
-		if(end)
-		{
-			node.end = true;
-		}
-		else
-		{
-			node = addToTrie(word.substring(1), node);
-		}
-	}
-	console.log(trie);
-	return trie;
-}
 
 for(word of corpus)
 {
-	trieCorpus = addToTrie(word, trieCorpus);
+	trieCorpus = trieCorpus.addWord(word);
 }
 console.log(trieCorpus);
 
@@ -135,20 +117,16 @@ function getSuggestions2(curr, patt, corp)
 
 function getTrieSuggestions(patt, node, f)
 {
-	console.log("pattern", patt);
 	let side = patt.charAt(0) == "1" ? node.ones : node.zeroes;
-	//console.log(node.val);
 	let sidearray = [];
 	let endarray = [];
 	if(side.size > 0)
 	{
 		sidearray = Array.from(side.values()).map((function(x) {return getTrieSuggestions(patt.substring(1), x, (function(y) {return f(y) + node.val}))})).flat();
-		//console.log("side", sidearray);
 	}
 	if(node.end)
 	{
 		endarray = [f(node.val)];
-		//console.log("end", endarray);
 	}
 	return sidearray.concat(endarray);
 }
